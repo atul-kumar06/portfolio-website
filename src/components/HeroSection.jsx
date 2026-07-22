841a128aab67e5950efaee08d060381445c97650
Atul <ramkanaujia63@gmail.com>
2026-07-22 22:34:30 +0530

    bug-fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,29 +4,33 @@ import React from "react";
 const HeroSection = () => {
   return (
     <section
-      className="flex flex-col items-center justify-center container mt-52 space-y-11"
+      className="min-h-screen flex flex-col  justify-center container relative px-4"
       id="hero"
     >
-      <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-        <span className="opacity-0 animate-fade-in">Hii I'm</span>{" "}
-        <span className="text-primary opacity-0 animate-fade-in-delay-1">
-          Atul
-        </span>
-        <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
-          👋
-        </span>
-      </h1>
-      <p className="text-xl md:text-xl text-muted-foreground max-w-3xl mx-auto opacity-0 animate-fade-in-delay-3 text-center leading-10 tracking-widest">
-        Turning complex ideas into high-performance web applications. As a
-        full-stack developer. I build engaging, user-centric interfaces from the
-        ground up.
-      </p>
-      <button className="pt-4 opacity-0 animate-fade-in-delay-4">
-        <a href="#projects" className="cosmic-button">
-          View My Work
-        </a>
-      </button>
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+      <div className="container max-w-4xl mx-auto text-center z-10">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            <span className="opacity-0 animate-fade-in">Hii I'm</span>{" "}
+            <span className="text-primary opacity-0 animate-fade-in-delay-1">
+              Atul
+            </span>
+            <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+              👋
+            </span>
+          </h1>
+          <p className="text-xl md:text-xl text-muted-foreground max-w-3xl mx-auto opacity-0 animate-fade-in-delay-3 text-center leading-10 tracking-widest z-10">
+            Turning complex ideas into high-performance web applications. As a
+            full-stack developer. I build engaging, user-centric interfaces from
+            the ground up.
+          </p>
+          <button className="pt-4 opacity-0 animate-fade-in-delay-4">
+            <a href="#projects" className="cosmic-button">
+              View My Work
+            </a>
+          </button>
+        </div>
+      </div>
+      <div className="absolute bottom-34 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
         <span className="text-sm text-muted-foreground mb-2">Scroll</span>
         <ArrowDown className="h-5 w-5 text-primary" />
       </div>
